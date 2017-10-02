b3e5d1c3587fe7be9d78079d7a5110f833a1340c
Stefanos1312 <32456667+Stefanos1312@users.noreply.github.com>
2017-10-02 14:55:29 +0100

    Add files via upload

---
+++ task 1 section 3.py
@@ -0,0 +1,11 @@
+print ("This program will add 2 different numbers and will find the results")
+#define numbers as integer
+number1 = int(input("please enter the first number"))
+number2 = int(input("please enter the second number"))
+print ("thank you")
+#now I will define the results by just adding '+' the numbers together
+result = number1 + number2
+print ("the result is")
+print (result)
+
+
